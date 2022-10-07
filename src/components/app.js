515c1cd7b3437d101984c30a5abb284da09b5d8b
Nastasia <lisitsinan@gmail.com>
2022-10-07 19:51:21 +0300

    add header basic layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import styles from './app.module.css';
+import AppHeader from './app-header/app-header';
 
 class App extends React.Component {
   render() {
     return (
-    <div>
-      test
-    </div>
+    <AppHeader />
     )
   }
 }
