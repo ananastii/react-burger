f0c39d71bf1ba9be59bd29cba205b952bfa74088
Nastasia <lisitsinan@gmail.com>
2022-10-07 16:37:35 +0300

    add app component main files

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './app.module.css';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      test
+    </div>
+    )
+  }
+}
+
+export default App;
