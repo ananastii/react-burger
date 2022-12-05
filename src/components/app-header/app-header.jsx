1febcad7362ec018289e96a11a1c7b1ac0faf52c
Nastasia <lisitsinan@gmail.com>
2022-12-05 19:39:01 +0200

    style fixes

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -14,15 +14,15 @@ const AppHeader = () => (
         </a>
         <a href="#" className={`pt-4 pr-5 pb-4 pl-5 ${styles.header__item}`}>
           <ListIcon type="secondary" />
-          <span className="ml-2 text text_type_main-default">Лента заказов</span>
+          <span className="ml-2 text text_type_main-default text_color_inactive">Лента заказов</span>
         </a>
       </div>
       <div className={`${styles.header__logo}`}>
         <Logo />
       </div>
       <a href="#" className={`pt-4 pr-5 pb-4 pl-5 ${styles.header__item} ${styles.menu_section_right}`}>
-        <ProfileIcon type="primary" />
-        <span className="ml-2 text text_type_main-default">Личный кабинет</span>
+        <ProfileIcon type="secondary" />
+        <span className="ml-2 text text_type_main-default text_color_inactive">Личный кабинет</span>
       </a>
     </nav>
   </header>
--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -7,7 +7,7 @@ const Ingredient = ({data, onImgClick}) => (
 
   <li className={styles.item}>
     <Counter className="counter-card" count={1} size="default" />
-    <img className={`ml-4 mr-4 mb-1`} src={data.image} alt={data.name} onClick={() => onImgClick(data)}></img>
+    <img className={`${styles.img} ml-4 mr-4 mb-1`} src={data.image} alt={data.name} onClick={() => onImgClick(data)}></img>
     <div className={`${styles.price} mb-1 text text_type_main-default`}>
       <span className="mr-2 text text_type_digits-default">{data.price}</span>
       <CurrencyIcon type="primary" />
