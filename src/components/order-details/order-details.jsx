9f9a7f97b71cff84569ddb12466d9dd9022b62c6
Nastasia <lisitsinan@gmail.com>
2022-12-05 17:01:02 +0200

    add order id text shadow

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -5,7 +5,7 @@ function OrderDetails () {
 
   return (
     <div className={`${styles.order} pt-30 pr-25 pb-30 pl-25`}>
-        <p className={`text text_type_digits-large mb-8`}>034536</p>
+        <p className={`${styles.id} text text_type_digits-large mb-8`}>034536</p>
         <p className={`text text_type_main-medium mb-15`}>идентификатор заказа</p>
         <div className={`${styles.img} mb-15`}></div>
         <p className={`text text_type_main-default mb-2`}>ваш заказ начали готовить</p>
