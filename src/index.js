f07b0044c6657858fb3257056c533d2d2f38952a
Nastasia <lisitsinan@gmail.com>
2023-01-05 22:12:33 +0200

    apply DevTools and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { compose } from 'redux';
 import './index.css';
 import './fonts/font.css';
 import App from './components/app/app';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
