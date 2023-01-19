9d24f5f7203f75618efc0d2e11c2fb03bc5c028b
Nastasia <lisitsinan@gmail.com>
2023-01-19 02:41:57 +0200

    select active tab during scroll

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './burger-ingredients.module.css';
 import IngredientsTab from '../ingredients-tab/ingredients-tab';
@@ -14,13 +14,6 @@ const BurgerIngredients = () => {
   const sauces = ingredients.filter((item) => item.info.type === 'sauce');
   const mains = ingredients.filter((item) => item.info.type === 'main');
 
-  const [currentTab, setCurrentTab] = useState('buns');
-
-  const clickOnTab = (id) => {
-    setCurrentTab(id);
-    document.querySelector(`#${id}`).scrollIntoView({behavior: 'smooth'});
-  }
-
   const dispatch = useDispatch();
   const { ingredient } = useSelector(store => store.ingredient);
 
@@ -28,15 +21,54 @@ const BurgerIngredients = () => {
     dispatch(closeIngredientDetails());
   };
 
+  // Navigation through tabs
+  const [currentTab, setCurrentTab] = useState('buns');
+  const [scrollEdge, setScrollEdge] = useState();
+
+  const handleTabClick = (id) => {
+    setCurrentTab(id);
+    document.querySelector(`#${id}`).scrollIntoView({behavior: 'smooth'});
+  }
+
+  const refTab = useRef();
+  const refBunsHeader = useRef();
+  const refSaucesHeader = useRef();
+  const refMainsHeader = useRef();
+
+  useEffect(() => {
+    setScrollEdge(refTab.current.getBoundingClientRect().bottom);
+  }, []);
+
+
+  const handleTabScroll = () => {
+    // for the perfect precision can use half of header's in calculations
+    let bunsHeaderDist = Math.abs(scrollEdge - refBunsHeader.current.getBoundingClientRect().top);
+    let saucesHeaderDist = Math.abs(scrollEdge - refSaucesHeader.current.getBoundingClientRect().top);
+    let mainsHeaderDist = Math.abs(scrollEdge - refMainsHeader.current.getBoundingClientRect().top);
+
+    const distancesAll = {
+      'buns': bunsHeaderDist,
+      'sauces': saucesHeaderDist,
+      'mains': mainsHeaderDist
+    };
+
+    const closestPos = Math.min(bunsHeaderDist, saucesHeaderDist, mainsHeaderDist);
+    const activeTabId = Object.keys(distancesAll).find(key => distancesAll[key] === closestPos);
+
+    setCurrentTab(activeTabId);
+  }
+
+
+
   return (
     <>
     <section className={`${styles.list} pl-5 pr-5`}>
       <h1 className={`pt-10 text text_type_main-large`}>Соберите бургер</h1>
-      <IngredientsTab setCurrent={clickOnTab} currentTab={currentTab}/>
-      <div className={`${styles.list__scroll} custom-scroll`}>
-        <IngredientsList title={'Булки'} data={buns} id={'buns'}/>
-        <IngredientsList title={'Соусы'} data={sauces} id={'sauces'}/>
-        <IngredientsList title={'Начинки'} data={mains} id={'mains'}/>
+      <IngredientsTab setCurrent={handleTabClick} currentTab={currentTab} refTab={refTab}/>
+      <div className={`${styles.list__scroll} custom-scroll`} onScroll={handleTabScroll}>
+        <IngredientsList title={'Булки'} data={buns} id={'buns'} refHeader={refBunsHeader}/>
+        <IngredientsList title={'Соусы'} data={sauces} id={'sauces'} refHeader={refSaucesHeader}/>
+        <IngredientsList title={'Начинки'} data={mains} id={'mains'} refHeader={refMainsHeader}/>
       </div>
     </section>
     {ingredient &&
--- src/components/ingredients-tab/ingredients-tab.jsx
+++ src/components/ingredients-tab/ingredients-tab.jsx
@@ -3,10 +3,10 @@ import styles from './ingredients-tab.module.css';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 
-function IngredientsTab({currentTab, setCurrent}) {
+function IngredientsTab({currentTab, setCurrent, refTab}) {
 
   return (
-    <div className={`${styles.tabs} mt-5 text_type_main-default`}>
+    <div className={`${styles.tabs} mt-5 text_type_main-default`} ref={refTab}>
       <Tab value="buns" active={currentTab === 'buns'} onClick={setCurrent}>
         Булки
       </Tab>
