75834baf3257e5271c5d0603969a732c335dcb40
Nastasia <lisitsinan@gmail.com>
2023-02-12 00:56:41 +0200

    delete ingredient details store and actions

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { ingredientsReducer } from './ingredients';
-import { ingredientDetailsReducer } from './ingredient-details';
 import { constructorReducer } from './burger-constructor';
 import { orderReducer } from './order';
 import { authReducer } from './auth';
 
 export const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
-  ingredient: ingredientDetailsReducer,
   burgerConstructor: constructorReducer,
   order: orderReducer,
   auth: authReducer,
