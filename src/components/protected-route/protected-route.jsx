9546e3174e92a4ff0a834d16201d7be5a52896c9
Nastasia <lisitsinan@gmail.com>
2023-02-14 13:16:25 +0200

    authorize user if there is at least refresh token

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,12 +1,24 @@
 import { Outlet, Navigate, useLocation} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { getUser } from '../../utils/store';
+import { getUserInfo } from '../../services/actions/auth';
+import { getCookie } from '../../utils/cookies';
 
 const ProtectedRouteElement = ({isUserAllowed}) => {
 
   const user = useSelector(getUser);
   const { state, pathname} = useLocation();
+  const refreshToken = getCookie("refreshToken");
 
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    if (!user && refreshToken) {
+      dispatch(getUserInfo());
+    }
+  }, [refreshToken, user, dispatch]);
 
   if (!user && isUserAllowed) {
     return (
